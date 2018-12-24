ae3cf5ec42088ca7bedcbe255661e1a9919fd6a1
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-12-24 23:43:31 +0200

    Initial commit

---
+++ lisp-math.js
@@ -0,0 +1,29 @@
+const negate = fn => val => !fn(val);
+const toInt = i => parseInt(i, 10);
+
+const mathFuncs = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b
+};
+
+const calculate = (operator, [head, ...tail]) =>
+  tail.reduce(mathFuncs[operator], head);
+
+const lispRegex = /\({1}([\+\-\*\/])\s([\s\-?\d]*)*\){1}/g;
+
+const lisp = str => {
+  const result = str.replace(lispRegex, (_, operator, numbers) =>
+    calculate(
+      operator,
+      numbers
+        .trim()
+        .split(" ")
+        .map(toInt)
+        .filter(negate(isNaN))
+    )
+  );
+
+  return isNaN(result) ? lisp(result) : result;
+};
