edd5ca1fa7a7126ddac9ffdaf9c23773061baf46
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-12-27 18:49:11 +0200

    Add tooling

--- lisp-math.js
+++ lisp-math.js
@@ -1,17 +1,17 @@
-const negate = fn => val => !fn(val);
-const toInt = i => parseInt(i, 10);
+const negate = fn => val => !fn(val)
+const toInt = i => parseInt(i, 10)
 
 const mathFuncs = {
-  "+": (a, b) => a + b,
-  "-": (a, b) => a - b,
-  "*": (a, b) => a * b,
-  "/": (a, b) => a / b
-};
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
+}
 
 const calculate = (operator, [head, ...tail]) =>
-  tail.reduce(mathFuncs[operator], head);
+  tail.reduce(mathFuncs[operator], head)
 
-const lispRegex = /\({1}([\+\-\*\/])\s([\s\-?\d]*)*\){1}/g;
+const lispRegex = /\({1}([\+\-\*\/])\s([\s\-?\d]*)*\){1}/g
 
 const lisp = str => {
   const result = str.replace(lispRegex, (_, operator, numbers) =>
@@ -19,11 +19,13 @@ const lisp = str => {
       operator,
       numbers
         .trim()
-        .split(" ")
+        .split(' ')
         .map(toInt)
-        .filter(negate(isNaN))
-    )
-  );
+        .filter(negate(isNaN)),
+    ),
+  )
 
-  return isNaN(result) ? lisp(result) : result;
-};
+  return isNaN(result) ? lisp(result) : result
+}
+
+export default lisp
